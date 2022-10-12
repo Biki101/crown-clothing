24dc63c81d1ed7de32ab6e2d09d35f4829eb00da
Biki <biki@core>
2022-10-12 15:52:34 +0545

    Error fix

--- src/component/directories/directories.component.jsx
+++ src/component/directories/directories.component.jsx
@@ -13,7 +13,7 @@ const Directories = () => {
       id: 2,
       title: "jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      route: "shop/jacktes",
+      route: "shop/jackets",
     },
     {
       id: 3,
